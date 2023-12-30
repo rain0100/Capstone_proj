60c3a380c255c594736a6221dc87f5107776fce3
mtoff <toffolom@mcmaster.ca>
2023-12-30 13:04:23 -0500

    Inventory toggle implemented

--- inventory/Inventory.gd
+++ inventory/Inventory.gd
@@ -2,11 +2,13 @@ extends PanelContainer
 
 const Slot= preload("res://inventory/slot.tscn")
 @onready var item_grid: GridContainer= $MarginContainer/GridContainer
-
+@onready var inventory_interface: PanelContainer= $"."
+signal toggle_inventory()
 
 func _ready() -> void:
 	var inv_data= preload("res://inventory/Inv.tres")
 	populate_item_grid(inv_data.slot_datas)
+	toggle_inventory.connect(toggle_inventory_interface)
 	
 func populate_item_grid(slot_datas: Array[SlotData])->void:
 	for child in item_grid.get_children():
@@ -18,3 +20,10 @@ func populate_item_grid(slot_datas: Array[SlotData])->void:
 		
 		if slot_data:
 			slot.set_slot_data(slot_data)
+			
+func _unhandled_input(event:InputEvent)->void:
+	if Input.is_action_just_pressed("toggle_inventory"):
+		toggle_inventory.emit()
+
+func toggle_inventory_interface() ->void:
+	inventory_interface.visible=not inventory_interface.visible
