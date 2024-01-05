94cd1af611482e21024c14941ac84883699b8ae6
ryanjohnson0100 <ryanjohnson114@gmail.com>
2024-01-05 15:42:27 -0500

    Not gate working
    implemented the not gate

--- Logic_pice/and_gate.gd
+++ Logic_pice/and_gate.gd
@@ -5,6 +5,6 @@ class_name AndGate
 
 func evaluate():
 	#this should not be a nand gate just to demenstat to jordan
-	outputs[0].set_state(!(inputs[0].get_state() and inputs[1].get_state()))
+	outputs[0].set_state((inputs[0].get_state() and inputs[1].get_state()))
 
 	
