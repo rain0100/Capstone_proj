7d587136b0d037c9d5092f88da1bed45a9d5bedc
ryanjohnson0100 <ryanjohnson114@gmail.com>
2024-01-06 13:58:30 -0500

    rename instene to wireobj

--- Logic_pice/and_gate.gd
+++ Logic_pice/and_gate.gd
@@ -6,5 +6,3 @@ class_name AndGate
 func evaluate():
 	#this should not be a nand gate just to demenstat to jordan
 	outputs[0].set_state((inputs[0].get_state() and inputs[1].get_state()))
-
-	
--- Logic_pice/nand_gate.gd
+++ Logic_pice/nand_gate.gd
@@ -4,5 +4,3 @@ class_name NandGate
 
 func evaluate():
 	outputs[0].set_state(!(inputs[0].get_state() and inputs[1].get_state()))
-
-	
