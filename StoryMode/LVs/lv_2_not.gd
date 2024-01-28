900197cdef78b5e7b6a0bfaedd64746d2bbc0ac9
ryanjohnson0100 <ryanjohnson114@gmail.com>
2024-01-27 20:14:59 -0500

    Got LVs 3, 4 and 6 working properly

--- StoryMode/LVs/lv_2_not.gd
+++ StoryMode/LVs/lv_2_not.gd
@@ -17,7 +17,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if not (levels[LV2]):
 		check_criteria()
 
