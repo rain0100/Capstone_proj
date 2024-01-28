e33a43d5bd302ec9ad24dc464d7b9f37571c75ec
ryanjohnson0100 <ryanjohnson114@gmail.com>
2024-01-27 21:15:46 -0500

    finished Lv 5
    there are some bugs that i found i will nead to go back and fix the:
    -if 2 wiers are conected to a input termanal you can only delet 1 you should not be able to gonect the second wire
    -i nead to add more stringet requierments for the pass fail of this level but for now it is good
    -add in feachers for devs like knowing what is conected to your gates

---
+++ StoryMode/LVs/lv_5_nand_nor.gd
@@ -0,0 +1,39 @@
+extends GeneralLevel
+
+class_name LevelFive
+
+@onready var level_input1 = $Toggle_Button
+@onready var level_input2 = $Toggle_Button2
+@onready var level_input3 = $Toggle_Button3
+@onready var nor_gate = $Nor_Gate
+@onready var nand_gate = $Nand_Gate
+@onready var level_output1 = $Input_terminal
+@onready var level_output2 = $Input_terminal2
+
+var LV5 : int = 4
+
+#keps track of if the criterea to pass the lv is met
+#if both are off
+var cri_off: bool = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if not (levels[LV5]):
+		check_criteria()
+
+func check_criteria():
+
+		if(!cri_off):
+			if(level_output1.connected_wire != null and level_output2.connected_wire != null):
+				if(nor_gate.inputs[0].connected_wire != null and nor_gate.inputs[1].connected_wire != null):
+					if(nand_gate.inputs[0].connected_wire != null and nand_gate.inputs[1].connected_wire != null):
+						if(not(level_output1.get_state() or level_output2.get_state())):
+							cri_off = true
+							print("you passed the test")
+							pass_level((LV5))
