0684b686bade889ae51b6e8f072a9d26b034148d
mtoff <toffolom@mcmaster.ca>
2024-01-29 20:18:42 -0500

    Main Menu added

---
+++ New Game Menu/Level_Select.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_creative_pressed():
+	get_tree().change_scene_to_file("res://creative.tscn")
+	
+
+
+func _on_menu_pressed():
+	get_tree().change_scene_to_file("res://Main Menu/Main_Menu.tscn")
+
+
+func _on_story_pressed():
+		get_tree().change_scene_to_file("res://StoryMode/story_mode.tscn")
