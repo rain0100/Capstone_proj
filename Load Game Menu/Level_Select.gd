ce1707719c97811d855c78131a036947238d0a5b
mtoff <toffolom@mcmaster.ca>
2024-02-03 15:40:18 -0500

    Save/Load

--- Load Game Menu/Level_Select.gd
+++ Load Game Menu/Level_Select.gd
@@ -1,6 +1,7 @@
 extends Control
 
 
+static var load_flag=0;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,14 +11,18 @@ func _ready():
 func _process(delta):
 	pass
 
-	
+
 func _on_creative_pressed():
 	get_tree().change_scene_to_file("res://creative.tscn")
-
+	
 
 func _on_menu_pressed():
 	get_tree().change_scene_to_file("res://Main Menu/Main_Menu.tscn")
 
 
 func _on_story_pressed():
-		get_tree().change_scene_to_file("res://StoryMode/story_mode.tscn")
+	load_flag=1;
+	get_tree().change_scene_to_file("res://StoryMode/story_mode.tscn")
+	
+		
+
--- Main Menu/Main_Menu.gd
+++ Main Menu/Main_Menu.gd
@@ -1,13 +1,18 @@
 extends Control
 
-
+var save_path="user://variable.save "
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 func _on_new_game_pressed():
 	get_tree().change_scene_to_file("res://New Game Menu/Level_Select.tscn")
-	 
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_load_game_pressed():
+	get_tree().change_scene_to_file("res://Load Game Menu/Level_Select.tscn")
+
+
