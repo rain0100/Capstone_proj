5f25dc3de546c14b82656ffa67131443f1fe23c2
Ginoth Kumar <ginothkumar@Ginoths-MacBook-Pro.local>
2024-02-05 13:36:46 -0500

    Level 5 beat bug fixed - check previous state of outputs

--- StoryMode/LVs/lv_5_nand_nor.gd
+++ StoryMode/LVs/lv_5_nand_nor.gd
@@ -15,6 +15,8 @@ var LV5 : int = 4
 #keps track of if the criterea to pass the lv is met
 #if both are off
 var cri_off: bool = false
+var pass_state1 : bool
+var pass_state2 : bool
 
 
 # Called when the node enters the scene tree for the first time.
@@ -28,12 +30,16 @@ func _process(_delta):
 		check_criteria()
 
 func check_criteria():
-
-		if(!cri_off):
-			if(level_output1.connected_wire != null and level_output2.connected_wire != null):
-				if(nor_gate.inputs[0].connected_wire != null and nor_gate.inputs[1].connected_wire != null):
-					if(nand_gate.inputs[0].connected_wire != null and nand_gate.inputs[1].connected_wire != null):
-						if(not(level_output1.get_state() or level_output2.get_state())):
+	if(!cri_off):
+		if(level_output1.connected_wire != null and level_output2.connected_wire != null):
+			if(nor_gate.inputs[0].connected_wire != null and nor_gate.inputs[1].connected_wire != null):
+				if(nand_gate.inputs[0].connected_wire != null and nand_gate.inputs[1].connected_wire != null):
+					if(not(level_output1.get_state() or level_output2.get_state())):
+						#purpose of this if statement is to check if current states are equal to state from previous frame
+						if(pass_state1 == level_output1.get_state() and pass_state2 == level_output2.get_state()):
 							cri_off = true
 							print("you passed the test")
 							pass_level((LV5))
+	
+	pass_state1 = level_output1.get_state()
+	pass_state2 = level_output2.get_state()
