43ebadb3125f84e6a1ef9ba92f1bc3ac7ad97037
Suhrullkeha <suhruulekha@gmail.com>
2024-03-19 15:47:59 -0400

    Working Save/Load

--- LOAD SAVED INV.gd
+++ LOAD SAVED INV.gd
@@ -36,16 +36,36 @@ func _on_pressed():
 		while not file.eof_reached():
 			var line = file.get_line().replace("[", "").replace("]", "")
 			if line != "":
-				var gate_name = line.split(":")[0].to_lower()
+				var gate_name = line.split("_")[0].to_lower()
 				gate_names.append(gate_name)
-	for gate in gate_names: 
-		var path = "res://Logic_pice/" + gate + ".tscn"
-		# var ItemClass = preload(path)	
-		# item = ItemClass.instantiate()
 
-			# if line: 
-			# 	for slot in gridCont.get_children():
-			# 		slot.item = line
-			# 		slot.slotPut(slot.item)
-			# 		print(slot.item)
+	#put them in their respective slots
+	for gate in gate_names: 
+		var path = "res://Logic_pice/" + gate + "_gate.tscn"
+		var ItemClass = load(path)   
+		var item = ItemClass.instantiate()
+		var slot_found = false
+		for slot in gridCont.get_children(): 
+			if slot.item == null:
+				slot.item = item
+				slot.slotPut(slot.item)
+				slot_found = true
+				break
+		if not slot_found:
+			print("No empty slots for", gate)
+
+
+
+	# var file = FileAccess.open(save_path, FileAccess.READ)
+	# var slots = gridCont.get_children()
+	# var index = 0
+	# while not file.eof_reached() and index < len(slots):
+	# 	var line = file.get_line().replace("[", "").replace("]", "") #an array of gates
+	# 	if line: 
+	# 		var slot = slots[index]
+	# 		slot.item = item
+	# 		#print(slot.item)
+	# 		slot.slotPut(slot.item)
+	# 		index +=1
+
 
